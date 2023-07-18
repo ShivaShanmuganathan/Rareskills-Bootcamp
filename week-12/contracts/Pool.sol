1e9bd3b90e60cf726cb6f55e49a9e8bbade7a64f
Shiva Shanmuganathan <shiva.shanmuganathan@srmuniv.edu.in>
2023-07-18 17:23:12 +0530

    Updated Pool Contract

--- week-12/contracts/Pool.sol
+++ week-12/contracts/Pool.sol
@@ -6,9 +6,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Pool is Ownable {
     IERC20 public token;
+    uint256 private reserve;
 
     address public pairContract;
     uint256 public constant maxApprovalAmount = type(uint256).max;
+
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdraw(address indexed withdrawer, uint256 amount);
 
     constructor(address _token) {
         token = IERC20(_token);
@@ -21,5 +25,33 @@ contract Pool is Ownable {
             token.approve(pairContract, maxApprovalAmount),
             "Approval failed"
         );
+    }
+
+    function deposit(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than 0");
+
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "Token transfer failed"
+        );
+
+        reserve += amount;
+
+        emit Deposit(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) external onlyOwner {
+        require(amount > 0, "Amount must be greater than 0");
+        require(reserve >= amount, "Insufficient reserves");
+
+        require(token.transfer(msg.sender, amount), "Token transfer failed");
+
+        reserve -= amount;
+
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function getReserve() external view returns (uint256) {
+        return reserve;
     }
 }
