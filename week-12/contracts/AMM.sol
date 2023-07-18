5a0c4c6d75aeb9ad55e3e89d5f31a1e7c34a2fc1
Shiva Shanmuganathan <shiva.shanmuganathan@srmuniv.edu.in>
2023-07-18 17:48:37 +0530

    Added AMM contract

---
+++ week-12/contracts/AMM.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface Pair {
+    function swapExactIn(
+        address tokenIn,
+        uint256 amountIn,
+        address tokenOut,
+        uint256 amountOutMin
+    ) external;
+
+    function swapExactOut(
+        address tokenIn,
+        uint256 amountInMax,
+        address tokenOut,
+        uint256 amountOut
+    ) external;
+}
+
+// interface Pool {
+//     function approvePairContract(address _pairContract) external;
+// }
+
+contract AMM is Ownable {
+    address public tokenA;
+    address public tokenB;
+    Pair public pair;
+
+    bool private isSwapping;
+
+    modifier nonReentrant() {
+        require(!isSwapping, "Reentrant call");
+        isSwapping = true;
+        _;
+        isSwapping = false;
+    }
+
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+
+    // function setPair(address pairAddress) external onlyOwner {
+    //     pair = Pair(pairAddress);
+    //     pair.setPairContract(address(this));
+    // }
+
+    function swapExactIn(
+        address tokenIn,
+        uint256 amountIn,
+        address tokenOut,
+        uint256 amountOutMin
+    ) external nonReentrant {
+        pair.swapExactIn(tokenIn, amountIn, tokenOut, amountOutMin);
+    }
+
+    function swapExactOut(
+        address tokenIn,
+        uint256 amountInMax,
+        address tokenOut,
+        uint256 amountOut
+    ) external nonReentrant {
+        pair.swapExactOut(tokenIn, amountInMax, tokenOut, amountOut);
+    }
+
+    // function approvePairContract(address poolAddress) external onlyOwner {
+    //     Pool(poolAddress).approvePairContract(poolAddress);
+    // }
+}
